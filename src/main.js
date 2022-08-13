1c1dad998cd3582e061ae4ca6ad936bcd2504e90
alexrod94 <alexrod94@gmail.com>
2022-08-13 11:34:09 +0200

    Solve syntax issues

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router/index";
-import pinia from "pinia";
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
 
 createApp(App).use(router).use(pinia).mount("#app");
