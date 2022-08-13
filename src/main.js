df6b563f545c4db6c6a06f8cee95c0d9044f39a5
alexrod94 <alexrod94@gmail.com>
2022-08-13 11:29:09 +0200

    Add router & pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router/index";
+import pinia from "pinia";
 
-createApp(App).mount('#app')
+createApp(App).use(router).use(pinia).mount("#app");
--- src/store/user.js
+++ src/store/user.js
@@ -1,22 +1,22 @@
 // /store/user.js
 
-import { defineStore } from 'pinia';
-import { supabase } from '../supabase';
+import { defineStore } from "pinia";
+import { supabase } from "../supabase";
 
-export const useUserStore = defineStore('user', {
+export const useUserStore = defineStore("user", {
   state: () => ({
     user: null,
   }),
 
   actions: {
-    async fetchUser () {
+    async fetchUser() {
       const user = await supabase.auth.user();
-      this.user = user
+      this.user = user;
     },
-    async signUp (email, password) {
+    async signUp(email, password) {
       const { user, error } = await supabase.auth.signUp({
         email: email,
-        password: password
+        password: password,
       });
       if (error) throw error;
       if (user) this.user = user;
@@ -25,9 +25,10 @@ export const useUserStore = defineStore('user', {
       enabled: true,
       strategies: [
         {
-          key: 'user',
-          storage: localStorage
-        }
-      ]
+          key: "user",
+          storage: localStorage,
+        },
+      ],
     },
+  },
 });
